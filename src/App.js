a8bbc064c028ff0fc07189dda246884bc1e84e8f
Eric Laursen <ericjlaursen@gmail.com>
2018-12-15 18:41:42 -0500

    loads a bit of state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+
+	constructor(props){
+		super(props)
+
+		this.state = {
+		    todos: [
+		      {
+		        text: "Learn about React",
+		        isCompleted: false
+		      },
+		      {
+		        text: "Meet friend for lunch",
+		        isCompleted: false
+		      },
+		      {
+		        text: "Build really cool todo app",
+		        isCompleted: false
+		      }
+		    ]
+		};
+	}
+
 	render(){
 		return (
 			<div>
