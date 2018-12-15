6cb21213d9a84cf7ba1899e48e84ad6e70953e6a
Eric Laursen <ericjlaursen@gmail.com>
2018-12-15 18:38:27 -0500

    converts App to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const App = () => {
-  return (
-    <div>
-      <p>Dummy React Component here!</p>
-    </div>
-  );
+
+class App extends React.Component {
+	render(){
+		return (
+			<div>
+				<p>Dummy React Component here!</p>
+			</div>
+		);	
+	}
 };
+
 export default App;
 ReactDOM.render(<App />, document.getElementById("app"));
