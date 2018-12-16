2066b97f3757dd2001e2bb993f33af4e7bbadb8c
Eric Laursen <ericjlaursen@gmail.com>
2018-12-15 19:34:14 -0500

    builds more complet form

--- src/components/AddToDoForm/index.js
+++ src/components/AddToDoForm/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './index.css';
 
-export default function PlaceHolder(props) {
-	return(
-		<p>ToDoForm</p>
-	);
+export default class AddToDoForm extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = { value: "" };	
+	}
+
+	updateValue(e){
+		this.setState({ value: e.target.value });
+	}
+
+	handleSubmit(e){
+		e.preventDefault();
+		if (!this.state.value) return;
+		this.props.addTodo(this.state.value);
+		this.setState({ value: "" });
+	};
+
+	render() {
+		const { value } = this.state;
+
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input
+					type="text"
+					className="input"
+					value={value}
+					onChange={this.updateValue}/>
+			</form>
+		);
+	}
 }
