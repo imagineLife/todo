11158bd2e54e693d88f947a31decc45a3db28c85
Eric Laursen <ericjlaursen@gmail.com>
2019-01-07 16:06:58 -0500

    Includes completeToDo

--- src/ToDoContext.js
+++ src/ToDoContext.js
@@ -9,13 +9,22 @@ class ToDoProvider extends React.Component{
 		todos: null
 	};
 
+	completeTodo = (index) => {
+		const newTodos = [...this.state.todos];
+		newTodos[index].isCompleted = true;
+		this.setState({ todos: newTodos });
+	};
+
 	componentDidMount(){
 		this.setState({todos: DUMMY_TODOS})		
 	}
 
 	render(){
 		return(
-			<Provider value={{ todos: this.state.todos }}>
+			<Provider value={{ 
+				todos: this.state.todos,
+				completeTodo: this.completeTodo
+			}}>
 				{this.props.children}
 			</Provider>
 
