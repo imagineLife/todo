04ff3a86f928d02a22d583d534f53d2a57d28402
Eric Laursen <ericjlaursen@gmail.com>
2019-01-06 20:32:57 -0500

    A Framework for ToDo Context

--- src/index.js
+++ src/index.js
@@ -4,5 +4,25 @@ import DefaultApp from "./DefaultApp";
 import ContextApp from "./ContextApp";
 import HooksApp from "./HooksApp";
 import ReduxApp from "./ReduxApp";
+import { ToDoProvider, ToDoConsumer } from "./ToDoContext.js"
 import style from "./main.css";
-ReactDOM.render(<ReduxApp />, document.getElementById("app"));
+
+// ReactDOM.render(<ReduxApp />, document.getElementById("app"));
+
+
+export default function Root(){
+	return(
+		<ToDoConsumer>
+			{({todos}) => 
+				<ContextApp />
+			}
+		</ToDoConsumer>
+	)
+}
+
+//IF Context, use THIS...
+ReactDOM.render(
+	<ToDoProvider>
+		<Root />
+	 </ToDoProvider>, 
+	document.getElementById("app"));
