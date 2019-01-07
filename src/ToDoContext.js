55f78acfbaef5db336befe20297666e3655ed13f
Eric Laursen <ericjlaursen@gmail.com>
2019-01-06 20:22:31 -0500

    basic context

---
+++ src/ToDoContext.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+//contains UserContext.Consumer && UserContext.Provider
+const { Provider, Consumer } = React.createContext()
+
+class ToDoProvider extends React.Component{
+	state = {
+		todos: null
+	};
+
+	componentDidMount(){
+		
+	}
+
+	render(){
+		return(
+			<Provider
+				value={{
+		          todos: this.state.todos
+		        }}>
+				{this.props.children}
+			</Provider>
+
+		)
+	}
+}
+
+export {ToDoProvider, Consumer as ToDoConsumer};
