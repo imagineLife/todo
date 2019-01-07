45228fe5e68b055284a4d5cffbae122d95a3a6c9
Eric Laursen <ericjlaursen@gmail.com>
2019-01-07 16:06:00 -0500

    Uses ToDoConsumer, and completeToDo Fn

--- src/components/SingleToDoContext/index.js
+++ src/components/SingleToDoContext/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './index.css';
+import { ToDoConsumer } from '../../ToDoContext';
 
 export default function SingleToDo(props) {
-	let { todo, index, completeToDo, removeToDo, txt } = props;
+	let { todo, index, removeToDo, txt } = props;
 	return(
 		<div
 			className="todo"
 			style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
 			{txt}
-			<button onClick={() => completeToDo(index)}>Complete</button>
-			<button onClick={() => removeToDo(index)}>x</button>
+
+			<ToDoConsumer>
+				{({completeTodo}) => 
+					<React.Fragment>
+						<button onClick={() => completeTodo(index)}>Complete</button>
+						<button onClick={() => removeToDo(index)}>x</button>
+					</React.Fragment>	
+				}
+			</ToDoConsumer>	
 		</div>
 	);
 }
