598e91bfc4e9d74307a33333e4077e9d88ef812b
Eric Laursen <ericjlaursen@gmail.com>
2019-01-06 20:22:52 -0500

    some dummy todo data

---
+++ src/dummyAPI.js
@@ -0,0 +1,18 @@
+export const DUMMY_TODOS = [
+	{
+		txt: 'Make Context section',
+		process: 'todo'
+	},
+	{
+		txt: 'Make Context AddToDo method',
+		process: 'todo'
+	},
+	{
+		txt: 'Make Context CompleteToDo method',
+		process: 'todo'
+	},
+	{
+		txt: 'Make Context RemoveToDo method',
+		process: 'todo'
+	}
+]
