db702bb5da9e8070905b4518910396f252976cea
Eric Laursen <ericjlaursen@gmail.com>
2019-01-06 19:58:35 -0500

    placeholder for a redux version

---
+++ src/ReduxApp.js
@@ -0,0 +1,80 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SingleToDo from './components/SingleToDo'
+import AddToDoForm from './components/AddToDoForm'
+import './main.css'
+
+class DefaultApp extends React.Component {
+
+	constructor(props){
+		super(props)
+
+		this.state = {
+		    todos: [
+		      {
+		        text: "Learn about React",
+		        isCompleted: false
+		      },
+		      {
+		        text: "Meet friend for lunch",
+		        isCompleted: false
+		      },
+		      {
+		        text: "Build really cool todo app",
+		        isCompleted: false
+		      }
+		    ]
+		};
+
+		this.completeTodo = this.completeTodo.bind(this)
+		this.removeTodo = this.removeTodo.bind(this)
+		this.addTodo = this.addTodo.bind(this)
+	}
+
+	completeTodo(index){
+		const newTodos = [...this.state.todos];
+		newTodos[index].isCompleted = true;
+		this.setState({ todos: newTodos });
+	};
+
+	removeTodo(index){
+		const newTodos = [...this.state.todos];
+		newTodos.splice(index, 1);
+		this.setState({ todos: newTodos });
+	};
+
+	addTodo(text){
+		const newTodos = [...this.state.todos, { text }];
+		this.setState({ todos: newTodos });
+	};
+
+	render(){
+
+		//destructure to-dos from state
+		const { todos } = this.state;
+			
+		let todoList = todos.map((t,ind) => {
+			return(
+				<SingleToDo
+					todo={t}
+					key={ind}
+					index={ind}
+					txt={t.text}
+					completeToDo={this.completeTodo}
+					removeToDo={this.removeTodo}
+				/>
+			)
+		})
+
+		return (
+			<div className="todoWrapper">
+				<div className="todo-list">
+					{todoList}
+					<AddToDoForm  addTodo={this.addTodo}/>
+				</div>	
+			</div>
+		);	
+	}
+};
+
+export default DefaultApp;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import DefaultApp from "./DefaultApp";
 import ContextApp from "./ContextApp";
 import HooksApp from "./HooksApp";
+import ReduxApp from "./ReduxApp";
 import style from "./main.css";
-ReactDOM.render(<ContextApp />, document.getElementById("app"));
+ReactDOM.render(<ReduxApp />, document.getElementById("app"));
