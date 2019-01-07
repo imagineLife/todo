5ea775066696f380f019332475665e3d461a6066
Eric Laursen <ericjlaursen@gmail.com>
2019-01-06 21:14:02 -0500

    code cleanup

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -5,26 +5,24 @@ import { ToDoConsumer } from '../ToDoContext';
 
 let ToDoList = (props) => {
 
-	let todoList = <ToDoConsumer>
-		{({todos}) =>
-			todos ?
-			todos.map((todo, ind) => {
-				return<SingleToDo
-					todo={todo}
-					key={ind}
-					index={ind}
-					txt={todo.txt}
-					completeToDo={props.completeTodo}
-					removeToDo={props.removeTodo}
-				/>;
-			}) : <div></div>
-		}
-	</ToDoConsumer>
-
 	return(
 		<div className="todoWrapper">
 			<div className="todo-list">
-				{todoList}
+				<ToDoConsumer>
+					{({todos}) =>
+						todos ?
+						todos.map((todo, ind) => {
+							return<SingleToDo
+								todo={todo}
+								key={ind}
+								index={ind}
+								txt={todo.txt}
+								completeToDo={props.completeTodo}
+								removeToDo={props.removeTodo}
+							/>;
+						}) : <div></div>
+					}
+				</ToDoConsumer>
 				<AddToDoForm  addTodo={props.addTodo}/>
 			</div>	
 		</div>
