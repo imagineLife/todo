b44678685e60e37688c83ce0e1dab766817180ac
Eric Laursen <ericjlaursen@gmail.com>
2019-01-07 20:45:43 -0500

    adds methods & vals to provider

--- src/ToDoContext.js
+++ src/ToDoContext.js
@@ -6,7 +6,8 @@ const { Provider, Consumer } = React.createContext()
 
 class ToDoProvider extends React.Component{
 	state = {
-		todos: null
+		todos: null,
+		newVal: ""
 	};
 
 	completeTodo = (index) => {
@@ -21,6 +22,20 @@ class ToDoProvider extends React.Component{
 		this.setState({ todos: newTodos });
 	};
 
+	addToDo = (text) => {
+		let newTodo = {
+			txt: text,
+			process: 'todo',
+			isComplete: false
+		};
+		const newTodos = [...this.state.todos, newTodo];
+		this.setState({ todos: newTodos, newVal: "" });
+	};
+
+	updateToDoVal = (newVal) => {		
+		this.setState({newVal: newVal})
+	}
+
 	componentDidMount(){
 		this.setState({todos: DUMMY_TODOS})		
 	}
@@ -30,7 +45,10 @@ class ToDoProvider extends React.Component{
 			<Provider value={{ 
 				todos: this.state.todos,
 				completeTodo: this.completeTodo,
-				removeTodo: this.removeTodo
+				removeTodo: this.removeTodo,
+				addToDo: this.addToDo,
+				updateToDoVal: this.updateToDoVal,
+				newVal: this.state.newVal
 			}}>
 				{this.props.children}
 			</Provider>
