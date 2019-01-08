b1c0cc2b6d3e197012576111d777f7bfd4ed02c5
Eric Laursen <ericjlaursen@gmail.com>
2019-01-07 20:44:28 -0500

    moves all logic to ToDoContext

--- src/ContextApp.js
+++ src/ContextApp.js
@@ -7,26 +7,10 @@ import ReactDOM from "react-dom";
 import ToDoList from './components/ToDoList'
 import './main.css'
 
-class ContextApp extends React.Component {
-
-	constructor(props){
-		super(props)
-
-		this.addTodo = this.addTodo.bind(this)
-	}
-
-	addTodo(text){
-		const newTodos = [...this.state.todos, { text }];
-		this.setState({ todos: newTodos });
-	};
-
-	render(){			
-
-		return (
-			<ToDoList
-				addTodo={this.addTodo}/>
-		);	
-	}
+const ContextApp = () => {		
+	return (
+		<ToDoList />
+	);	
 };
 
 export default ContextApp;
