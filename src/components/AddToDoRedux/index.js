377927207cce2b2f57a11709bf62e25235c64552
Eric Laursen <ericjlaursen@gmail.com>
2019-01-14 13:11:20 -0500

    redux components

---
+++ src/components/AddToDoRedux/index.js
@@ -0,0 +1,15 @@
+import { connect } from "react-redux"
+import { Input } from "../Input"
+import { addTodo } from "./actions"
+
+const AddTodo = connect(
+  // map state to props
+  state => ({}),
+  // map dispatch to props
+  dispatch => ({
+    // onSubmit prop triggers addTodo action
+    onSubmit: title => dispatch(addTodo(title)),
+  })
+)(Input)
+
+export default AddTodo
