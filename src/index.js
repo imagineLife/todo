465bfab5574296a6136295619292f08c68819d7b
Eric Laursen <ericjlaursen@gmail.com>
2019-01-15 17:30:31 -0500

    adds space for redux-specific app

--- src/index.js
+++ src/index.js
@@ -7,23 +7,38 @@ import ReduxApp from "./ReduxApp";
 import { ToDoProvider, ToDoConsumer } from "./ToDoContext.js"
 import style from "./main.css";
 
+//Redux-Specific
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './state/reducers/index'
+
+
+//ALL-BUT Redux Here
 // ReactDOM.render(<ReduxApp />, document.getElementById("app"));
 
 
-
-//IF Context, use THESE...
-export default function Root(){
-	return(
-		<ToDoConsumer>
-			{({todos}) => 
-				<ContextApp />
-			}
-		</ToDoConsumer>
-	)
-}
+//REDUX-SPECIFIc
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-	<ToDoProvider>
-		<Root />
-	 </ToDoProvider>, 
-	document.getElementById("app"));
+  <Provider store={store}>
+    <ReduxApp />
+  </Provider>, document.getElementById("app"));
+
+
+//IF Context, use THESE...
+// export default function Root(){
+// 	return(
+// 		<ToDoConsumer>
+// 			{({todos}) => 
+// 				<ContextApp />
+// 			}
+// 		</ToDoConsumer>
+// 	)
+// }
+
+// ReactDOM.render(
+// 	<ToDoProvider>
+// 		<Root />
+// 	 </ToDoProvider>, 
+// 	document.getElementById("app"));
