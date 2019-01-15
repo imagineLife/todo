98f2eff7f8110087bc1a5e92851eab7ee3d235ae
Eric Laursen <ericjlaursen@gmail.com>
2019-01-15 17:31:14 -0500

    Updates ToDoListRedux

--- src/containers/ToDoListRedux/index.js
+++ src/containers/ToDoListRedux/index.js
@@ -1,30 +1,24 @@
-import { connect } from 'react-redux'
-import { toggleTodo } from '../../state/actions'
-import TodoList from '../../components/TodoList'
-import { VisibilityFilters } from '../../state/actions'
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReduxTodo from '../../components/SingleToDoRedux'
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(t => t.completed)
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(t => !t.completed)
-    default:
-      throw new Error('Unknown filter: ' + filter)
-  }
+const TodoList = ({ todos, toggleTodo }) => (
+  <ul>
+    {todos.map(todo => (
+      <ReduxTodo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
+    ))}
+  </ul>
+)
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  toggleTodo: PropTypes.func.isRequired
 }
 
-const mapStateToProps = state => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter)
-})
-
-const mapDispatchToProps = dispatch => ({
-  toggleTodo: id => dispatch(toggleTodo(id))
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoList)
+export default TodoList
